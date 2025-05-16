5d3400e2834384aed5c48f1d3ddb6244d944ff87
caddu333 <cadualmansa@gmail.com>
2025-05-16 09:39:44 -0300

    Add files via upload

---
+++ CADU/Aulas-HTML-CSS-main/Aulas-HTML-CSS-main/HTML/aula15/main.js
@@ -0,0 +1,14 @@
+//**CRIANDO MEU PRIMEIRO SCRIPT - 1 linha
+
+/*CRIANDO MEU PRIMEIRO SCRIPT - 2 linha
+**CRIANDO MEU PRIMEIRO SCRIPT - 3 linha   
+***CRIANDO MEU PRIMEIRO SCRIPT - 4 linha
+****CRIANDO MEU PRIMEIRO SCRIPT - 5 linha
+*/
+
+var nome = "Trem BALA"; // Variável do tipo string - texto 
+let idade = 20; // Variável do tipo number - número
+const IdadeMinima = 18; // Variável do tipo number - número 
+var nome = "Cadu"; 
+
+// console.log(nome) 
